760e1a7622ffab33e18a2627794fc4f7f873b775
Tiendat <bryce.gazelle2000@gmail.com>
2019-11-12 13:56:26 +0700

    abc

---
+++ AddBorder/AddBorder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        string[] addBorder(string[] picture)
+        {
+            List<string> answer = new List<string>();
+            string decoratedRow = string.Empty;
+            for (int i = 0; i < picture[0].Length + 2; i++)
+            {
+                decoratedRow += "*";
+            }
+            answer.Add(decoratedRow);
+            for (int i = 0; i < picture.Length; i++)
+            {
+                string row = "*";
+                for (int j = 0; j < picture[0].Length; j++)
+                {
+                    row += picture[i][j];
+                }
+                row += "*";
+                answer.Add(row);
+            }
+            answer.Add(decoratedRow);
+            return answer.ToArray();
+        }
+
+        
+    }
+}
